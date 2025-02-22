1460877902317c075b37997df6b3005149fe544e
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-02-22 14:19:44 -0500

    Update location.py

--- location.py
+++ location.py
@@ -6,10 +6,8 @@ class location:
         self.cville = cville
 
 
-marieB = location("Marie Bette", "105 E Water St Charlottesville, VA 22902", )
-kardinal = location("Kardinal Hall", "722 Preston Ave #101, Charlottesville, VA 22903", )
-dairy = location("Dairy Market", "946 Grady Ave, Charlottesville, VA 22903", )
-lawn = location("The Lawn", "400 Emmet St S, Charlottesville, VA 22903", )
-end_games = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", )
-moores_creek = location("Moores Creek Park", "Moores Creek Park, Charlottesville, VA 22902")
-        
+marieB = location("Marie Bette", "105 E Water St Charlottesville, VA 22902","walk", "Bus 7" )
+kardinal = location("Kardinal Hall", "722 Preston Ave #101, Charlottesville, VA 22903", "Green Line", "Bus 9" )
+dairy = location("Dairy Market", "946 Grady Ave, Charlottesville, VA 22903", "Green Line", "Bus 9")
+lawn = location("The Lawn", "400 Emmet St S, Charlottesville, VA 22903", "Yellow Line, Green Line", "")
+endGames = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", "walk", "Bus 7")
