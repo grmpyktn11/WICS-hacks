be51c930d12f6ad5aa4415168010b5fb98276148
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-02-22 13:48:32 -0500

    initial commit

---
+++ examplePosts.py
@@ -0,0 +1,4 @@
+import Post
+import location
+marieBette = maire
+maire = Post("Kyle","img\mariebette-cafe-and-bakery-logo.jpg", "I had a great time at Marie Bette cafe! We ate 100 croissonts while we hung out", "#cafe", )
---
+++ search.py
@@ -0,0 +1,14 @@
+import location
+import Post
+
+
+
+listOfPosts = []
+def search():
+    print("hey")
+
+def search(searchTerm):
+    print("hello")
+
+if __name__ == '__main__':
+    search();
