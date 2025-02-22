8cf983278a160a9b48f38ea5c1b28b894b46a65c
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-02-22 14:15:07 -0500

    Update location.py

--- location.py
+++ location.py
@@ -1,15 +1,14 @@
 class location:
-    def __init__(self, name, location, uva = "", cville = ""):
+    def __init__(self, name, address, uva = "", cville = "", bus = None):
         self.name = name
-        self.location = location
+        self.address = address
         self.uva = uva
         self.cville = cville
+        self.bus = bus
 
 
-marieB = location("Marie Bette", "105 E Water St Charlottesville, VA 22902", )
-kardinal = location("Kardinal Hall", "722 Preston Ave #101, Charlottesville, VA 22903", )
-dairy = location("Dairy Market", "946 Grady Ave, Charlottesville, VA 22903", )
-lawn = location("The Lawn", "400 Emmet St S, Charlottesville, VA 22903", )
-endGames = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", )
-
-        
+marieB = location("Marie Bette", "105 E Water St Charlottesville, VA 22902","walk", "Bus 7" )
+kardinal = location("Kardinal Hall", "722 Preston Ave #101, Charlottesville, VA 22903", "Green Line", "Bus 9" )
+dairy = location("Dairy Market", "946 Grady Ave, Charlottesville, VA 22903", "Green Line", "Bus 9")
+lawn = location("The Lawn", "400 Emmet St S, Charlottesville, VA 22903", "Yellow Line, Green Line", "")
+endGames = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", "walk", "Bus 7")
