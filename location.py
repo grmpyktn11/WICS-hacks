c4b9063c1c5c80fb4bd82eeeec9d6d40755e0c22
blairezh <113472440+blairezh@users.noreply.github.com>
2025-02-22 14:38:57 -0500

    sample posts

--- location.py
+++ location.py
@@ -10,4 +10,5 @@ marieB = location("Marie Bette", "105 E Water St Charlottesville, VA 22902","wal
 kardinal = location("Kardinal Hall", "722 Preston Ave #101, Charlottesville, VA 22903", "Green Line", "Bus 9" )
 dairy = location("Dairy Market", "946 Grady Ave, Charlottesville, VA 22903", "Green Line", "Bus 9")
 lawn = location("The Lawn", "400 Emmet St S, Charlottesville, VA 22903", "Yellow Line, Green Line", "")
-endGames = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", "walk", "Bus 7")
+endgames = location("The End Games", "390 Hillsdale Dr, Charlottesville, VA 22901", "walk", "Bus 7")
+moorescreek = location("Moores Creek", "Moores Creek Park, Charlottesville, VA 22903")
