41f587203370ab7d33636b299e92f035851f015e
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-02-22 13:16:27 -0500

    Post class

---
+++ Post.py
@@ -0,0 +1,10 @@
+class Post:
+    def __init__(self,username,picture,content,hashtag,location):
+        self.username = username
+        self.picture = picture
+        self.content = content
+        self.hashtag = hashtag
+        self.location = location
+        pass
+
+    
