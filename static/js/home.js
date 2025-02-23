6033adf8e6594174e378f9cea0d6e53d9fbbb682
gckang <76260739+gckang@users.noreply.github.com>
2025-02-23 14:40:35 -0500

    edit cover

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector(".sticky-header");
     let coverHeight = document.querySelector(".cover").offsetHeight;
 
-    if (window.scrollY > coverHeight/2 ) {
+    if (window.scrollY > coverHeight/5 ) {
         header.classList.add("visible");
     } else {
         header.classList.remove("visible");
