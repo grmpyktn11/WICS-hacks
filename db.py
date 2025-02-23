07724289bd4dfb000a56e4a61f8231a6546f40dc
khalid <71997967+grmpyktn11@users.noreply.github.com>
2025-02-23 11:34:17 -0500

    Features added in dicord.

--- db.py
+++ db.py
@@ -25,7 +25,7 @@ def db_initialize():
 
     #populate location table
     connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('Marie Bette', '105 E Water St, Charlottesville, VA 22902', 'walk', 'Bus 7' )")
-    connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('Kardinal Hall', '722 Preston Ave #101, Charlottesville, VA 22903', 'Green Line', 'Bus 9' )")
+    connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('KardinalHall', '722 Preston Ave #101, Charlottesville, VA 22903', 'Green Line', 'Bus 9' )")
     connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('Dairy Market', '946 Grady Ave, Charlottesville, VA 22903', 'Green Line', 'Bus 9')")
     connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('The Lawn', '400 Emmet St S, Charlottesville, VA 22903', 'Yellow Line, Green Line', '')")
     connection.execute("INSERT INTO location (name, address, uva, cville) VALUES ('The End Games', '390 Hillsdale Dr, Charlottesville, VA 22901', 'walk', 'Bus 7')")
@@ -33,14 +33,15 @@ def db_initialize():
 
     #populate post table
     connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('kyler5442', 'img/mariebette.JPG', 'love this cafe!!!! always come here on tuesday', 'Marie Bette')")
-    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('klye76666', 'img/kardinal hall.JPG', 'Perfect place to sit outside and enjoy the vibe in on a breeze sunny day. Miss my wife.', 'Kardinal Hall')")
-    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('kaile1112', 'img/dairy market.JPG', 'BOOO!!!!!! Im not a fan of this place because I got food poisoning from the ramen store >:T', 'Dairy Market')")
+    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('klye76666', 'img/kardinal_hall.JPG', 'Perfect place to sit outside and enjoy the vibe in on a breeze sunny day. Miss my wife.', 'Kardinal Hall')")
+    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('kaile1112', 'img/dairy_market.JPG', 'BOOO!!!!!! Im not a fan of this place because I got food poisoning from the ramen store >:T', 'Dairy Market')")
     connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('evilraidersxxXx', 'img/the_lawn.JPG', 'so many bug bites in the summer but great place for a picnic', 'The Lawn')")
     connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('MissKylie', 'img/the_end_games.JPG', 'yeah dawg me and my boys always pu to play hmu if ur chill like that', 'The End Games')")
-    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('charlottesvilleresidentlover10', 'img/moores creek.JPG', 'i like to go here to scream. sometimes.', 'Moores Creek')")
+    connection.execute("INSERT INTO post (username, picture, content, locationname) Values ('charlottesvilleresidentlover10', 'img/moores_creek.JPG', 'i like to go here to scream. sometimes.', 'Moores Creek')")
     connection.commit()
 
-db_initialize()
+
+# db_initialize()
 print("Locations")
 cursor.execute("SELECT * FROM location")
 for row in cursor.fetchall():
